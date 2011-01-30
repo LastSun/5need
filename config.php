9cec6309f01dbe19999c6512a38a0b84228b5d32
zhangya998@gmail.com <zhangya998@gmail.com@fb9216ac-b904-4a2d-3154-568a92de8ace>
2011-01-30 13:38:22 +0000

    ph界面改为div布局

--- config.php
+++ config.php
@@ -1,29 +1,28 @@
 <?php 
-$mysql_host		= "localhost";
-
-$mysql_user		= "root";
-
-$mysql_pass		= "admin";
-
-$mysql_dbname	= "5need";
-
-$mysql_prefix	= "rushui_";
-
-$dir_inc		= "include/";
-
-$linktablename  = "rushui_link";
-
-$schoolname		= "USTC";
-
-$dirlogo		= "/upload/smallimg/";
-
-$dirboss		= "../";
-
-$link 			= mysql_connect($mysql_host,$mysql_user,$mysql_pass) or ('连接数据库失败！'.mysql_error());
-
-mysql_select_db($mysql_dbname,$link);
-
-mysql_query("SET NAMES UTF8");
-
+	$mysql_host		= "localhost";
+	
+	$mysql_user		= "root";
+	
+	$mysql_pass		= "admin";
+	
+	$mysql_dbname	= "5need";
+	
+	$mysql_prefix	= "rushui_";
+	
+	$dir_inc		= "include/";
+	
+	$linktablename  = "rushui_link";
+	
+	$schoolname		= "USTC";
+	
+	$dirlogo		= "/upload/smallimg/";
+	
+	$dirboss		= "../";
+	
+	$link 			= mysql_connect($mysql_host,$mysql_user,$mysql_pass) or ('连接数据库失败！'.mysql_error());
+	
+	mysql_select_db($mysql_dbname,$link);
+	
+	mysql_query("SET NAMES UTF8");
 
 ?>
--- project/user/personaluser/personal homepage/ph.php
+++ project/user/personaluser/personal homepage/ph.php
@@ -2,43 +2,43 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link type="text/css" href="ph.css" rel="stylesheet" />
 <title>个人主页</title>
 </head>
 
 <body>
-<table width="100%" border="1">
-  <tr>
-    <td colspan="2">留言板</td>
-  </tr>
-  <tr>
-    <td colspan="2">个人信息</td>
-  </tr>
+	<div id="messages">
+    	<p>留言板</p>
+ 	</div>
 
-	<?php
-			include '../../../../config.php';
-	
-    		$mysql_tbname	= "personaluser";
-			if(!mysql_select_db($mysql_dbname,$link)) echo ('连接数据表失败！'.mysql_error());
-			$query="SELECT * FROM personaluser"; 
-			$result=mysql_query($query);
-			$myrow=mysql_fetch_array($result);
-			for($i=0;$i<mysql_num_fields($result);$i++)   
-    		{   
-		         $name=mysql_field_name($result,$i);
-				 echo "<tr><td width='30'>".$name."</td>";
-		         echo "<td>".$myrow["$name"]."</td></tr>";
-    		}   
-
-
-	?>
+	<div id="personal">
+    	<p>个人信息</p>
+    	<table>
+			<?php
+					include '../../../../config.php';
+			
+		    		$mysql_tbname	= "personaluser";
+					if(!mysql_select_db($mysql_dbname,$link)) echo ('连接数据表失败！'.mysql_error());
+					$query="SELECT * FROM personaluser"; 
+					$result=mysql_query($query);
+					$myrow=mysql_fetch_array($result);
+					for($i=0;$i<mysql_num_fields($result);$i++)   
+		    		{   
+				         $name=mysql_field_name($result,$i);
+						 echo "<tr><td width='30' class='info_name'>".$name."</td>";
+				         echo "<td class='info_info'>".$myrow["$name"]."</td></tr>";
+		    		}   
+			?>
+		</table>
+	</div>
     
-  <tr>
-    <td colspan="2">个人动态</td>
-  </tr>
-  <tr>
-    <td colspan="2">活动经历</td>
-  </tr>
-</table>
+ 	<div id="personalaction">
+    	<p>个人动态</p>
+	</div>
+  
+	<div id="actionmessage">
+    	<p>活动经历</p>
+  	</div>
 
 </body>
 </html>
